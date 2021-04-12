89a38ff8b7de60fffe783daa9b94a01912bedcff
dumasenko16 <79253909+dumasenko16@users.noreply.github.com>
2021-04-12 12:06:37 +0300

    Add files via upload

--- signup_action.php
+++ signup_action.php
@@ -1,35 +1,36 @@
 <?php
-$dbname = 'ToDoList';
-$email = $_POST['email'];
-$pass = $_POST['password'];;
-$passhash = md5($pass);
-$newtekst = 'Введите вашу задачу';
-$con = mysqli_connect("localhost","root","", $dbname);
-		$sql = "INSERT INTO users (name, password, task) VALUES ('$email', '$passhash', '$newtekst' )";
+$dbname = 'todolist';
+$name = $_POST['name'];
+$pass = $_POST['password'];
 
-			if ((re_add($con, $email, $passhash)==True)){
+$con = mysqli_connect("localhost","root","", $dbname);
+		$sql = "INSERT INTO users (name, password) VALUES ('$name', '$pass')";
+
+			if ((re_add($con, $name, $pass)==True)){
 			$reg_mistake = "password or login error";
             include('signup.html');
             echo "$reg_mistake";
             exit();
 		}else{
-			if (mysqli_query($con, $sql)) {
-				include('index.html');
-            	header('index.html');
+			 if (mysqli_query($con, $sql))
+                         {
+                            include('login.html');
+                            header('login.html');
 
-		}
-}
+		
+                         }
+                }
 
-		function re_add($con, $email, $psw)
+		function re_add($con, $login, $psw)
 {
 	
-    $dbname = 'ToDoList';
+    $dbname = 'todolist';
 	$con = mysqli_connect("localhost","root","", $dbname);
-	$sql_find = "SELECT name FROM media_users WHERE password = '$psw'  OR name = '$email'";
+	$sql_find = "SELECT name FROM users WHERE password = '$psw'  OR name = '$login'";
 	$result_find = mysqli_query($con, $sql_find);
 	if (mysqli_num_rows($result_find)>0) {
 		return True;
 }
 	return False;
 	}
-	?>
+?>
